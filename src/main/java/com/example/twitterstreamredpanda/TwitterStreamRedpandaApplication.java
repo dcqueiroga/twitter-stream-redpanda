2a84c609f8a2c413c5038bf729387faec82bac59
Daniel Queiroga <daniel.queiroga@radixeng.com.br>
2021-10-31 23:37:26 -0300

    Initial commit

---
+++ src/main/java/com/example/twitterstreamredpanda/TwitterStreamRedpandaApplication.java
@@ -0,0 +1,18 @@
+package com.example.twitterstreamredpanda;
+
+import com.example.twitterstreamredpanda.messaging.TwitterBinder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableBinding(TwitterBinder.class)
+@EnableScheduling
+public class TwitterStreamRedpandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterStreamRedpandaApplication.class, args);
+    }
+
+}
